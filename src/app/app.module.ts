3a175cacabfb82cd28c62faf16c061c2960a779b
Jordan Ludovissie <Jludovissie24@gmail.com>
2022-05-05 20:28:15 -0500

    Angular Project Created
    Home, Nav & Sign up Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home.component';
+import { NavBarComponent } from './NavBar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
